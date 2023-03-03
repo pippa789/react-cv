937484e9f39b328f06a1263c109ac858132a1ba3
unknown <allenpip341@gmail.com>
2023-03-03 19:46:54 +0000

    some more tinkering with the css. router working as it should

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,26 +8,25 @@ justify-content:center;
 align-items:center;
 background-color:black;
 width:100%;
-height:100px;
+height:50px;
 color: #ffffff;
-text-decoration: none;
+
 `;
 
 const LinkStyle = styled.h1`
 background-color: black;
 color: #ffffff;
-
-line-style:none;
 font-family: 'Phudu', cursive;
 padding: 10px 70px;
+text-decoration: none;
 `;
 function NavBar() {
   return (
     
     <LinkContainer>
-      <LinkStyle><Link to="/react-cv/">Home</Link></LinkStyle>
-      <LinkStyle><Link to="/react-cv/about">About</Link></LinkStyle>
-      <LinkStyle><Link to="/react-cv/Contact">Contact me</Link></LinkStyle>
+      <LinkStyle><Link className='linkstyle' to="/react-cv/">Home</Link></LinkStyle>
+      <LinkStyle><Link className='linkstyle' to="/react-cv/about">About</Link></LinkStyle>
+      <LinkStyle><Link className='linkstyle' to="/react-cv/Contact">Contact me</Link></LinkStyle>
     </LinkContainer>
   )
 }
