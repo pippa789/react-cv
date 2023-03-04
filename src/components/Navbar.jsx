983923964534936fc268692667a948d6f6984d75
unknown <allenpip341@gmail.com>
2023-03-04 11:32:31 +0000

    tidying up and tweaking css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,22 @@ import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 const LinkContainer = styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-background-color:black;
-width:100%;
-height:50px;
-color: #ffffff;
-
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  // background-color:black;
+  width:100%;
+  height:50px;
 `;
 
-const LinkStyle = styled.h1`
-background-color: black;
-color: #ffffff;
-font-family: 'Phudu', cursive;
-padding: 10px 70px;
-text-decoration: none;
-`;
+
 function NavBar() {
   return (
     
     <LinkContainer>
-      <LinkStyle><Link className='linkstyle' to="/react-cv/">Home</Link></LinkStyle>
-      <LinkStyle><Link className='linkstyle' to="/react-cv/about">About</Link></LinkStyle>
-      <LinkStyle><Link className='linkstyle' to="/react-cv/Contact">Contact me</Link></LinkStyle>
+      <Link className='linkstyle' to="/react-cv/">Home</Link>
+      <Link className='linkstyle' to="/react-cv/about">About</Link>
+      <Link className='linkstyle' to="/react-cv/Contact">Contact me</Link>
     </LinkContainer>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,30 @@ import styled from 'styled-components';
 
 
 
-const Title = styled.div`
+const Welcome = styled.div`
 color: white;
 font-size: 100px;
 font-family: 'Phudu', cursive;
 background: black;
 `;
 
+const Name = styled.h1`
+color: white;
+font-size:75px;
+`;
+
+const TagLine = styled.div`
+color:white;
+font-size:50px;
+`;
+
 function Home() {
   return (
     <>
-    <Title>Welcome, My name is Pip</Title>
-    <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
-    labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi
-    animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
-    aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia
-    pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
-    commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa
-    proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
-    eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-    Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et
-    culpa duis.</p>
+    <Welcome>Welcome</Welcome> 
+   <Name> My name is Pip</Name>
+   <TagLine>I'm a Jr developer looking for a front end role.</TagLine>
+    
    </>
   )
 }
